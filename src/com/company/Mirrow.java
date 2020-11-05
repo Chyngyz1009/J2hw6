6051e678e0e8624015bd0bc87b2bea9993966fc8
Chyngyz1009 <chika_0410@inbox.ru>
2020-11-05 22:20:42 +0600

    Initial commit

---
+++ src/com/company/Mirrow.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Mirrow  {
+
+     void print();
+
+
+}
---
+++ src/com/company/Window.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class Window<T> extends Number implements Mirrow {
+
+    private T number;
+
+    public Window(T number) {
+        this.number = number;
+    }
+
+    public T getNumber() {
+        return number;
+    }
+
+    public void setNumber(T number) {
+        this.number = number;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("number"+getNumber());
+
+    }
+
+    @Override
+    public int intValue() {
+        return 0;
+    }
+
+    @Override
+    public long longValue() {
+        return 0;
+    }
+
+    @Override
+    public float floatValue() {
+        return 0;
+    }
+
+    @Override
+    public double doubleValue() {
+        return 0;
+    }
+}
+
